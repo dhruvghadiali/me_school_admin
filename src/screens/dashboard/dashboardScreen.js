d9441c7c4c441dc182ccdd37f4f52049809beffd
dhruv ghadiali <dhruvghadiali@hotmail.com>
2024-12-21 03:12:39 +0530

    test commit for alias name

--- src/screens/dashboard/dashboardScreen.js
+++ src/screens/dashboard/dashboardScreen.js
@@ -1,9 +1,11 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+
 import { summary } from "../../slice/dashboard/dashboardAction";
 import { resetState } from "../../slice/dashboard/dashboardSlice";
 import AuthHoc from "../../components/common/authHoc/authHoc";
-import MESidebar from "../../components/common/sidebar/meSidebar";
+import MESidebar from "@/src/components/screens/dashboard/header/header";
+// import DashboardScreenHeader from "../../components/screens/dashboard/header/header";
 import DashboardScreenHeader from "../../components/screens/dashboard/header/header";
 import DashboardScreenSummary from "../../components/screens/dashboard/summary/summary";
 import DashboardScreenChartSummary from "../../components/screens/dashboard/chartSummary/chartSummary";
