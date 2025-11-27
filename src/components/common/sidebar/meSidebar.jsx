c9bcec1395ee60ffdf5b1776f8c296c0773996f7
dhruv ghadiali <dhruvghadiali@hotmail.com>
2025-11-28 02:21:59 +0530

    Updated sidebar

--- src/components/common/sidebar/meSidebar.jsx
+++ src/components/common/sidebar/meSidebar.jsx
@@ -24,9 +24,8 @@ import {
   SidebarTrigger,
   SidebarInset,
 } from "@MEShadcnComponents/sidebar";
+import { Separator } from "@MEShadcnComponents/separator";
 import { sidebarMenuLabel, sidebar } from "@MELocalization/en";
-
-import MEButton from "@MECommonComponents/form/button/meButton";
 import PropTypes from "prop-types";
 import _ from "lodash";
 
@@ -46,75 +45,86 @@ const MESidebar = ({ children }) => {
   };
 
   return (
-    <SidebarProvider>
-      <Sidebar className="shadow-xl shadow-dark">
-        <SidebarContent>
-          <SidebarHeader className="h-20 bg-danger p-2 justify-center items-center" />
-          <SidebarGroup className="h-screen">
-            <SidebarGroupLabel className="mr-5 truncate ...">
+    <SidebarProvider defaultOpen={true}>
+      <Sidebar collapsible="offcanvas" className="border-r">
+        <SidebarHeader className="border-b bg-danger">
+          <div className="flex h-full items-center px-4">
+            <h2 className="text-white font-bold text-lg">
               {t('titleStatic', { defaultValue: sidebar.titleStatic })}
+            </h2>
+          </div>
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupLabel className="px-2 text-xs uppercase text-sidebar-foreground/70">
+              Menu
             </SidebarGroupLabel>
             <SidebarGroupContent>
               <SidebarMenu>
                 {sidebarMenu.map((item) => (
                   <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild>
-                      <MEButton
-                        variant={"ghost"}
-                        className={`justify-start cursor-pointer hover:cursor-pointer ${
-                          item.title === activeMenu
-                            ? "text-secondary bg-dark hover:text-danger"
-                            : "text-dark"
-                        } `}
-                        onClick={() => onClick(item)}
-                      >
-                        <item.icon />
-                        <span>
-                          {_.startCase(
-                            t(item.title, {
-                              defaultValue: sidebarMenuLabel[item.title],
-                            })
-                          )}
-                        </span>
-                      </MEButton>
+                    <SidebarMenuButton
+                      onClick={() => onClick(item)}
+                      isActive={item.title === activeMenu}
+                      className={`cursor-pointer ${
+                        item.title === activeMenu
+                          ? "bg-dark text-secondary"
+                          : ""
+                      }`}
+                    >
+                      <item.icon className="h-4 w-4" />
+                      <span>
+                        {_.startCase(
+                          t(item.title, {
+                            defaultValue: sidebarMenuLabel[item.title],
+                          })
+                        )}
+                      </span>
                     </SidebarMenuButton>
                   </SidebarMenuItem>
                 ))}
               </SidebarMenu>
             </SidebarGroupContent>
           </SidebarGroup>
-          <SidebarFooter>
-            <SidebarMenu>
-              {footerMenu.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <MEButton
-                      variant={"ghost"}
-                      className="justify-start text-dark cursor-pointer hover:cursor-pointer"
-                      onClick={() => onClick(item)}
-                    >
-                      <item.icon />
-                      <span>
-                        {_.upperFirst(
-                          t(item.title, {
-                            defaultValue: sidebarMenuLabel[item.title],
-                          })
-                        )}
-                      </span>
-                    </MEButton>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarFooter>
         </SidebarContent>
+        <SidebarFooter className="mt-auto border-t">
+          <SidebarMenu>
+            {footerMenu.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton
+                  onClick={() => onClick(item)}
+                  className="cursor-pointer"
+                >
+                  <item.icon className="h-4 w-4" />
+                  <span>
+                    {_.upperFirst(
+                      t(item.title, {
+                        defaultValue: sidebarMenuLabel[item.title],
+                      })
+                    )}
+                  </span>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarFooter>
       </Sidebar>
-      <main className="w-full ">
-        <SidebarTrigger />
-        <div className="flex ml-5">
+      <SidebarInset className="flex flex-col">
+        <header className="sticky top-0 z-10 flex h-14 shrink-0 items-center gap-2 border-b bg-background px-4">
+          <SidebarTrigger />
+          <Separator orientation="vertical" className="h-4" />
+          <h1 className="text-base font-semibold">
+            {_.upperFirst(
+              t(activeMenu, {
+                defaultValue: sidebarMenuLabel[activeMenu],
+              })
+            )}
+          </h1>
+        </header>
+        <main className="flex-1 overflow-auto p-6">
           {children}
-        </div>
-      </main>
+        </main>
+      </SidebarInset>
     </SidebarProvider>
   );
 };
